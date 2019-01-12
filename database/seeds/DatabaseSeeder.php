b3461fdfe39e6826208f9f3a2076e62a2fe9c4b5
xiayin <1084581212@qq.com>
2019-01-12 16:32:48 +0800

    修改数据填充

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(AdminsTableSeeder::class);
-         $this->call(ProductsTableSeeder::class);
+         //$this->call(ProductsTableSeeder::class);
          //$this->call(SpreadTableSeeder::class);
         //$this->call(UsersTableSeeder::class);
     }
