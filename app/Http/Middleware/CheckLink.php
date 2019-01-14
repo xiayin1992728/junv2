473a8a28e40c621461d6e24994032e3c6193f0a9
xiayin <1084581212@qq.com>
2019-01-14 11:54:14 +0800

    添加审核页面

--- app/Http/Middleware/CheckLink.php
+++ app/Http/Middleware/CheckLink.php
@@ -35,6 +35,8 @@ class CheckLink
             // 得到当前页面在执行顺序中的 位置
             $mark = array_search($url,$pages);
             // 遍历执行顺序 对比 每个页面是否有权限
+            dump($pages);
+            dd($auth);
             foreach ($pages as $k => $v) {
                 if ($k == $mark && in_array($url,$auth)) {
                     if ($idcard && $url == config('app.url').'/verify') {
