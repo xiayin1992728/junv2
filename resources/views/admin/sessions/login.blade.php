c68166b2b542228e504a1bf9062732442a23e8fa
vagrant <vagrant@homestead>
2019-02-12 09:51:03 +0000

    修改后台登陆页面

--- resources/views/admin/sessions/login.blade.php
+++ resources/views/admin/sessions/login.blade.php
@@ -1,56 +1,70 @@
-@include('admin.layouts._meta')
-<meta name="csrf-token" content="{{ csrf_token() }}">
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+          content="Flat Dark Web Login Form Responsive Templates, Iphone Widget Template, Smartphone login forms,Login form, Widget Template, Responsive Templates, a Ipad 404 Templates, Flat Responsive Templates"/>
+    <!--webfonts-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="/css/admin/style.css">
+    <!--//webfonts-->
+    <script src="/layui/js/jquery.min.js"></script>
 </head>
-<body class="login-bg">
-<div class="login layui-anim layui-anim-up">
-    <div class="message">后台管理</div>
-    <div id="darkbannerwrap"></div>
-    <form method="post" class="layui-form" >
-        <input name="phone" placeholder="手机号"  type="text" lay-verify="required" class="layui-input" >
-        <hr class="hr15">
-        <input name="password" lay-verify="required" placeholder="密码"  type="password" class="layui-input">
-        <hr class="hr15">
-        <input value="登录" lay-submit lay-filter="login" style="width:100%;" type="submit">
-        <hr class="hr20" >
+<body>
+<!--SIGN UP-->
+<h1>钱富贵后台登陆</h1>
+<div class="login-form">
+    <div class="close"></div>
+    <div class="head-info">
+        <label class="lbl-1"> </label>
+        <label class="lbl-2"> </label>
+        <label class="lbl-3"> </label>
+    </div>
+    <div class="clear"></div>
+    <div class="avtar">
+        <img src="/static/admin/login/avtar.png"/>
+    </div>
+    <form>
+        <input type="text" id="phone" class="text" value="" placeholder="请输入手机号">
+        <div class="key">
+            <input type="password" id="password" value="" placeholder="请输入密码">
+        </div>
     </form>
+    <div class="signin">
+        <input type="submit" id="singin" value="登陆">
+    </div>
 </div>
-@include('admin.layouts._footer')
+</body>
+</html>
 <script>
-    $(function  () {
-        layui.use('form', function(){
-            var form = layui.form;
-            form.on('submit(login)', function(data){
-                console.log(data.field);
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-                $.ajax({
-                   url:"{{ route('session.store') }}",
-                   type:'post',
-                   data:data.field,
-                   dataType:'json',
-                   success:function(res) {
-                       if (res.status == 200) {
-                           location.href = "{{ route('admin.home') }}"
-                       } else {
-                           layer.msg(res.msg,{icon:5});
-                       }
-                   },
-                   errors:function(res) {
-
-                   }
-                });
-                return false;
-            });
+    $('#singin').on('click',function(){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
         });
-    })
+        let data = {phone:$('#phone').val(),password:$('#password').val()};
+        $.ajax({
+            url: "{{ route('session.store') }}",
+            type: 'post',
+            data: data,
+            dataType: 'json',
+            success: function (res) {
+                if (res.status == 200) {
+                    location.href = "{{ route('admin.home') }}"
+                } else {
+                    alert(res.msg);
+                }
+            },
+            errors: function (res) {
 
-    if (window != top ) {
+            }
+        });
+    });
+
+    if (window != top) {
         top.location.href = window.location.href;
     }
 </script>
 <!-- 底部结束 -->
-</body>
-</html>
